175e8f6dbb30a07d345098175669fedff2d2564c
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2017-09-29 21:38:23 +0330

    Fix doctrine mapping errors

--- src/AppBundle/Model/Entity/Traits/AttachmentTrait.php
+++ src/AppBundle/Model/Entity/Traits/AttachmentTrait.php
@@ -8,12 +8,31 @@
 
 namespace AppBundle\Model\Entity\Traits;
 
-use Symfony\Component\Validator\constraints as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait AttachmentTrait
 {
     /**
      * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     * @Assert\File(
+     *     mimeTypes = {
+     *         "application/pdf",
+     *         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+     *         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+     *         "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+     *         "application/msword",
+     *         "application/vnd.ms-excel",
+     *         "application/vnd.ms-powerpoint",
+     *         "application/vnd.ms-access",
+     *         "image/bmp",
+     *         "image/jpeg",
+     *         "image/png",
+     *         "image/tiff",
+     *         "image/vnd.adobe.photoshop",
+     *         "image/vnd.dwg"
+     *     }
+     * )
      */
     private $location;
 
