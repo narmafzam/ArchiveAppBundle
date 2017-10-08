f2bf41af1e7cdd23af2fe9df40bbe81eb91f8c46
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2017-10-08 19:25:39 +0330

    Update service configuraton

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Narmafzam\ArchiveBundle\NarmafzamArchiveBundle(),
+            new ArchiveAppBundle\ArchiveAppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/ArchiveAppBundle/ArchiveAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ArchiveAppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArchiveAppBundle extends Bundle
+{
+}
