e26f201d383bc67708904833cecc744036b9bfaa
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2017-10-11 13:18:56 +0330

    Using Symfony Encore for asset management

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .enableVersioning()
+
+    // directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', './web/assets/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
