333b3936938c2ea40a591bc53a62591483eee9c5
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2017-10-24 16:38:41 +0330

    Add JMS-TranslationBundle as a dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Narmafzam\ArchiveBundle\NarmafzamArchiveBundle(),
             new ArchiveAppBundle\ArchiveAppBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
