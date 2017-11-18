b84d989a2874f67aa68ce697811726b36a312a33
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2017-11-18 21:31:50 +0330

    Move assets directorry to project root

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,10 @@ Encore
     })
 
     // will output as web/build/app.js
-    .addEntry('app', './web/assets/js/main.js')
+    .addEntry('app', './assets/js/main.js')
 
     // will output as web/build/global.css
-    .addStyleEntry('global', './web/assets/css/global.scss')
+    .addStyleEntry('global', './assets/css/global.scss')
 
     .enableSassLoader(function(sassOptions) {}, {
         resolveUrlLoader: false
